a741f6a197e463ce5a8026f58af01423b37e0505
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-05-13 22:57:13 +0800

    epic-rsshub

--- epic/epic.js
+++ epic/epic.js
@@ -5,32 +5,25 @@ fetchFreeGames().then(() => {
 async function fetchFreeGames() {
   const request = Request();
   const { body } = await request.get({
-    url: "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=US&allowCountries=US",
+    url: "https://rsshub.app/epicgames/freegames",
   });
-  const data = JSON.parse(body);
-  const now = new Date();
-  const items = data.data.Catalog.searchStore.elements.filter(
-    (item) =>
-      item.promotions &&
-      item.promotions.promotionalOffers &&
-      item.promotions.promotionalOffers[0] &&
-      new Date(
-        item.promotions.promotionalOffers[0].promotionalOffers[0].startDate
-      ) <= now &&
-      new Date(
-        item.promotions.promotionalOffers[0].promotionalOffers[0].endDate
-      ) >= now
-  );
-  items.forEach((item) => {
-    console.log(`🎮 [Epic 限免]  ${item.title}`);
-    console.log(`⏰ 发布时间: ${item.effectiveDate}`);
-    console.log(`📰 游戏简介: ${item.description}`);
+  const itemRegex = new RegExp(/<item>[\s\S]*?<\/item>/g);
+  body.match(itemRegex).forEach((item) => {
+    let name = item.match(/<title><!\[CDATA\[([\s\S]*?)\]\]><\/title>/)[1];
+    let url = item.match(/<link>([\s\S]*?)<\/link>/)[1];
+    let time = item.match(/<pubDate>([\s\S]*?)<\/pubDate>/)[1];
+    let description = item.match(
+      /<description><!\[CDATA\[<p>([\s\S]+?)<\/p>/
+    )[1];
+    console.log(`🎮 [Epic 限免]  ${name}`);
+    console.log(`⏰ 发布时间: ${formatTime(time)}`);
+    console.log(`📰 游戏简介: ${description}`);
     $notification.post(
-      `🎮 [Epic 限免]  ${item.title}`,
-      `⏰ 发布时间: ${item.effectiveDate}`,
-      `📰 游戏简介: ${item.description}`,
+      `🎮 [Epic 限免]  ${name}`,
+      `⏰ 发布时间: ${formatTime(time)}`,
+      `📰 游戏简介: ${description}`,
       {
-        url: `https://store.epicgames.com/zh-CN/p/${item.catalogNs.mappings[0].pageSlug}`,
+        url,
       }
     );
   });
@@ -39,9 +32,6 @@ async function fetchFreeGames() {
 function Request() {
   return new (class {
     request(obj, method) {
-      obj.headers = {};
-      obj.headers["User-Agent"] =
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36";
       return new Promise((resolve) => {
         $httpClient[method](obj, (err, resp, body) => {
           resolve({
@@ -59,4 +49,11 @@ function Request() {
       return this.request(obj, "post");
     }
   })();
+}
+
+function formatTime(timestamp) {
+  const date = new Date(timestamp);
+  return `${date.getFullYear()}年${
+    date.getMonth() + 1
+  }月${date.getDate()}日${date.getHours()}时`;
 }
