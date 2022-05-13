e733935e1bc4b568541845574445156d2f23cda5
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-05-13 21:21:41 +0800

    格式化

--- acfun/acfun.js
+++ acfun/acfun.js
@@ -1,88 +1,83 @@
 const cookieKey = "cookie-acfun";
 const title = "Acfun";
 
-if (typeof $request !== 'undefined') {
-    getCookie();
-    $done();
+if (typeof $request !== "undefined") {
+  getCookie();
+  $done();
 } else {
-    signIn().then(() => $done());
+  signIn().then(() => $done());
 }
-
 
 function getCookie() {
-    console.log('开始获取cookie');
-    let msg = "写入cookie失败\n配置错误, 无法读取请求头";
-    if ($request.headers) {
-        const regex = /acPasstoken=.+?; auth_key=.+?;/;
-        let cookie = $request.headers['Cookie'] ? $request.headers['Cookie'] : "";
-        cookie = regex.exec(cookie);
-        if (cookie !== null) {
-            cookie = cookie[0];
-            const oldCookie = $persistentStore.read(cookieKey);
-            msg = oldCookie ? "更新" : "写入";
-            if (cookie != oldCookie) {
-                const status = $persistentStore.write(cookie, cookieKey);
-                msg += status ? 'Cookie成功 🎉' : 'Cookie失败‼️';
-            } else {
-                $done();
-                return;
-            }
-        }
+  console.log("开始获取cookie");
+  let msg = "写入cookie失败\n配置错误, 无法读取请求头";
+  if ($request.headers) {
+    const regex = /acPasstoken=.+?; auth_key=.+?;/;
+    let cookie = $request.headers["Cookie"] ? $request.headers["Cookie"] : "";
+    cookie = regex.exec(cookie);
+    if (cookie !== null) {
+      cookie = cookie[0];
+      const oldCookie = $persistentStore.read(cookieKey);
+      msg = oldCookie ? "更新" : "写入";
+      if (cookie != oldCookie) {
+        const status = $persistentStore.write(cookie, cookieKey);
+        msg += status ? "Cookie成功 🎉" : "Cookie失败‼️";
+      } else {
+        $done();
+        return;
+      }
     }
-    $notification.post(title, "", msg);
+  }
+  $notification.post(title, "", msg);
 }
 
-
 async function signIn() {
-    const request = Request();
-    const cookie = $persistentStore.read(cookieKey);
-    const req = {
-        url: 'https://www.acfun.cn/rest/pc-direct/user/signIn',
-        headers: {
-            Cookie: cookie,
-            'User-Agent': 'AcFun/6.62.0 (iPhone; iOS 15.4.1; Scale/3.00)',
-            'Accept': 'application/json'
-        }
-    };
-    const {
-        err,
-        body
-    } = await request.get(req);
-    let msg = '';
-    if (err) {
-        msg = `请求失败!\n${err}`;
+  const request = Request();
+  const cookie = $persistentStore.read(cookieKey);
+  const req = {
+    url: "https://www.acfun.cn/rest/pc-direct/user/signIn",
+    headers: {
+      Cookie: cookie,
+      "User-Agent": "AcFun/6.62.0 (iPhone; iOS 15.4.1; Scale/3.00)",
+      Accept: "application/json",
+    },
+  };
+  const { err, body } = await request.get(req);
+  let msg = "";
+  if (err) {
+    msg = `请求失败!\n${err}`;
+  } else {
+    const data = JSON.parse(body);
+    if (data.result == 0) {
+      msg = data.msg;
+    } else if (data.error_msg) {
+      msg = data.error_msg;
     } else {
-        const data = JSON.parse(body);
-        if (data.result == 0) {
-            msg = data.msg;
-        } else if (data.error_msg) {
-            msg = data.error_msg;
-        } else {
-            msg = "签到失败，cookie过期" + body;
-        }
+      msg = "签到失败，cookie过期" + body;
     }
-    console.log(msg);
-    $notification.post(title, "", msg);
+  }
+  console.log(msg);
+  $notification.post(title, "", msg);
 }
 
 function Request() {
-    return new(class {
-        request(obj, method) {
-            return new Promise(resolve => {
-                $httpClient[method](obj, (err, resp, body) => {
-                    resolve({
-                        err,
-                        resp,
-                        body
-                    })
-                })
-            })
-        }
-        get(obj) {
-            return this.request(obj, 'get');;
-        }
-        post(obj) {
-            return this.request(obj, 'post');
-        }
-    })();
+  return new (class {
+    request(obj, method) {
+      return new Promise((resolve) => {
+        $httpClient[method](obj, (err, resp, body) => {
+          resolve({
+            err,
+            resp,
+            body,
+          });
+        });
+      });
+    }
+    get(obj) {
+      return this.request(obj, "get");
+    }
+    post(obj) {
+      return this.request(obj, "post");
+    }
+  })();
 }
--- epic/epic.js
+++ epic/epic.js
@@ -1,52 +1,62 @@
 fetchFreeGames().then(() => {
-    $done()
+  $done();
 });
 
 async function fetchFreeGames() {
-    const request = Request();
-    const {
-        body
-    } = await request.get({
-        url: "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=US&allowCountries=US"
-    });
-    const data = JSON.parse(body);
-    const now = new Date();
-    const items = data.data.Catalog.searchStore.elements.filter(item => item.promotions && item.promotions.promotionalOffers && item.promotions.promotionalOffers[0] && new Date(item.promotions.promotionalOffers[0].promotionalOffers[0].startDate) <= now && new Date(item.promotions.promotionalOffers[0].promotionalOffers[0].endDate) >= now)
-    items.forEach((item) => {
-        console.log(`🎮 [Epic 限免]  ${item.title}`);
-        console.log(`⏰ 发布时间: ${item.effectiveDate}`);
-        console.log(`📰 游戏简介: ${item.description}`);
-        $notification.post(
-            `🎮 [Epic 限免]  ${item.title}`,
-            `⏰ 发布时间: ${item.effectiveDate}`,
-            `📰 游戏简介: ${item.description}`, {
-                url: `https://store.epicgames.com/zh-CN/p/${item.catalogNs.mappings[0].pageSlug}`
-            }
-        );
-    })
-
+  const request = Request();
+  const { body } = await request.get({
+    url: "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=US&allowCountries=US",
+  });
+  const data = JSON.parse(body);
+  const now = new Date();
+  const items = data.data.Catalog.searchStore.elements.filter(
+    (item) =>
+      item.promotions &&
+      item.promotions.promotionalOffers &&
+      item.promotions.promotionalOffers[0] &&
+      new Date(
+        item.promotions.promotionalOffers[0].promotionalOffers[0].startDate
+      ) <= now &&
+      new Date(
+        item.promotions.promotionalOffers[0].promotionalOffers[0].endDate
+      ) >= now
+  );
+  items.forEach((item) => {
+    console.log(`🎮 [Epic 限免]  ${item.title}`);
+    console.log(`⏰ 发布时间: ${item.effectiveDate}`);
+    console.log(`📰 游戏简介: ${item.description}`);
+    $notification.post(
+      `🎮 [Epic 限免]  ${item.title}`,
+      `⏰ 发布时间: ${item.effectiveDate}`,
+      `📰 游戏简介: ${item.description}`,
+      {
+        url: `https://store.epicgames.com/zh-CN/p/${item.catalogNs.mappings[0].pageSlug}`,
+      }
+    );
+  });
 }
 
 function Request() {
-    return new(class {
-        request(obj, method) {
-            obj.headers = {};
-            obj.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36';
-            return new Promise(resolve => {
-                $httpClient[method](obj, (err, resp, body) => {
-                    resolve({
-                        err,
-                        resp,
-                        body
-                    })
-                })
-            })
-        }
-        get(obj) {
-            return this.request(obj, 'get');;
-        }
-        post(obj) {
-            return this.request(obj, 'post');
-        }
-    })();
+  return new (class {
+    request(obj, method) {
+      obj.headers = {};
+      obj.headers["User-Agent"] =
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36";
+      return new Promise((resolve) => {
+        $httpClient[method](obj, (err, resp, body) => {
+          resolve({
+            err,
+            resp,
+            body,
+          });
+        });
+      });
+    }
+    get(obj) {
+      return this.request(obj, "get");
+    }
+    post(obj) {
+      return this.request(obj, "post");
+    }
+  })();
 }
