0b3e209d45f90225c05bc044ff1caa8307837e4d
KotaHv <92137267+KotaHv@users.noreply.github.com>
2022-09-26 02:08:08 +0800

    fix ua

--- Scripts/jd.js
+++ Scripts/jd.js
@@ -78,7 +78,7 @@ async function jdBean() {
     },
     body: "functionId=signBeanIndex&appid=ld",
   };
-  await $.http.post(req).then(({ body }) => {
+  await $.http.post(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (data.code == 3) {
       msg = "京东商城-京豆: 失败, 原因: Cookie失效‼️";
@@ -129,7 +129,7 @@ async function jrSubsidy() {
     },
     body: BODY,
   };
-  await $.http.post(req).then(({ body }) => {
+  await $.http.post(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (
       data.resultCode == 0 &&
@@ -169,7 +169,7 @@ async function jdCash() {
       Cookie: COOKIE,
     },
   };
-  await $.http.get(req).then(({ body }) => {
+  await $.http.get(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (data.data.success && data.data.result) {
       msg = `京东商城-现金: 成功, 明细: ${data.data.result.signCash || `无`
@@ -201,7 +201,7 @@ async function jrBeanDouble() {
       `{"actCode":"F68B2C3E71","type":3,"frontParam":{"belong":"jingdou"}}`
     )}`,
   };
-  await $.http.post(req).then(({ body }) => {
+  await $.http.post(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (data.resultCode == 0) {
       if (data.resultData.data.businessData != null) {
@@ -243,7 +243,7 @@ async function TotalCash() {
     },
     body: "body=%7B%22fp%22%3A%22-1%22%2C%22appToken%22%3A%22apphongbao_token%22%2C%22childActivityUrl%22%3A%22-1%22%2C%22country%22%3A%22cn%22%2C%22openId%22%3A%22-1%22%2C%22childActivityId%22%3A%22-1%22%2C%22applicantErp%22%3A%22-1%22%2C%22platformId%22%3A%22appHongBao%22%2C%22isRvc%22%3A%22-1%22%2C%22orgType%22%3A%222%22%2C%22activityType%22%3A%221%22%2C%22shshshfpb%22%3A%22-1%22%2C%22platformToken%22%3A%22apphongbao_token%22%2C%22organization%22%3A%22JD%22%2C%22pageClickKey%22%3A%22-1%22%2C%22platform%22%3A%221%22%2C%22eid%22%3A%22-1%22%2C%22appId%22%3A%22appHongBao%22%2C%22childActiveName%22%3A%22-1%22%2C%22shshshfp%22%3A%22-1%22%2C%22jda%22%3A%22-1%22%2C%22extend%22%3A%22-1%22%2C%22shshshfpa%22%3A%22-1%22%2C%22activityArea%22%3A%22-1%22%2C%22childActivityTime%22%3A%22-1%22%7D&client=apple&clientVersion=8.5.0&d_brand=apple&networklibtype=JDNetworkBaseAF&openudid=1fce88cd05c42fe2b054e846f11bdf33f016d676&sign=fdc04c3ab0ee9148f947d24fb087b55d&st=1581245397648&sv=120",
   };
-  await $.http.post(req).then(({ body }) => {
+  await $.http.post(jdRequest(req)).then(({ body }) => {
     if (body.match(/(\"totalBalance\":\d+)/)) {
       const data = JSON.parse(body);
       msg = `京东-总红包: ${data.totalBalance}`;
@@ -265,7 +265,7 @@ async function TotalBean() {
       Cookie: COOKIE,
     },
   };
-  await $.http.get(req).then(({ body }) => {
+  await $.http.get(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (data.msg == "success" && data.retcode == 0) {
       msg = `京东-总京豆: ${data.data.assetInfo.beanNum}`;
@@ -289,7 +289,7 @@ async function TotalSubsidy() {
         "https://active.jd.com/forever/cashback/index?channellv=wojingqb",
     },
   };
-  await $.http.get(req).then(({ body }) => {
+  await $.http.get(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (data.resultCode == 0 && data.resultData && data.resultData.data) {
       msg = `京东-总金贴查: ${data.resultData.data.balance}`;
@@ -311,7 +311,7 @@ async function TotalMoney() {
       Cookie: COOKIE,
     },
   };
-  await $.http.get(req).then(({ body }) => {
+  await $.http.get(jdRequest(req)).then(({ body }) => {
     const data = JSON.parse(body);
     if (
       data.code == 0 &&
@@ -330,5 +330,10 @@ async function TotalMoney() {
   return msg;
 }
 
+function jdRequest(request) {
+  request.headers["User-Agent"] =
+    "JD4iPhone/167169 (iPhone; iOS 13.4.1; Scale/3.00)";
+  return request
+}
 
 function ENV() { const e = "function" == typeof require && "undefined" != typeof $jsbox; return { isQX: "undefined" != typeof $task, isLoon: "undefined" != typeof $loon, isSurge: "undefined" != typeof $httpClient && "undefined" != typeof $utils, isBrowser: "undefined" != typeof document, isNode: "function" == typeof require && !e, isJSBox: e, isRequest: "undefined" != typeof $request, isScriptable: "undefined" != typeof importModule } } function HTTP(e = { baseURL: "" }) { const { isQX: t, isLoon: s, isSurge: o, isScriptable: n, isNode: i, isBrowser: r } = ENV(), u = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/; const a = {}; return ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"].forEach(h => a[h.toLowerCase()] = (a => (function (a, h) { h = "string" == typeof h ? { url: h } : h; const d = e.baseURL; d && !u.test(h.url || "") && (h.url = d ? d + h.url : h.url), h.body && h.headers && !h.headers["Content-Type"] && (h.headers["Content-Type"] = "application/x-www-form-urlencoded"); const l = (h = { ...e, ...h }).timeout, c = { onRequest: () => { }, onResponse: e => e, onTimeout: () => { }, ...h.events }; let f, p; if (c.onRequest(a, h), t) f = $task.fetch({ method: a, ...h }); else if (s || o || i) f = new Promise((e, t) => { (i ? require("request") : $httpClient)[a.toLowerCase()](h, (s, o, n) => { s ? t(s) : e({ statusCode: o.status || o.statusCode, headers: o.headers, body: n }) }) }); else if (n) { const e = new Request(h.url); e.method = a, e.headers = h.headers, e.body = h.body, f = new Promise((t, s) => { e.loadString().then(s => { t({ statusCode: e.response.statusCode, headers: e.response.headers, body: s }) }).catch(e => s(e)) }) } else r && (f = new Promise((e, t) => { fetch(h.url, { method: a, headers: h.headers, body: h.body }).then(e => e.json()).then(t => e({ statusCode: t.status, headers: t.headers, body: t.data })).catch(t) })); const y = l ? new Promise((e, t) => { p = setTimeout(() => (c.onTimeout(), t(`${a} URL: ${h.url} exceeds the timeout ${l} ms`)), l) }) : null; return (y ? Promise.race([y, f]).then(e => (clearTimeout(p), e)) : f).then(e => c.onResponse(e)) })(h, a))), a } function API(e = "untitled", t = !1) { const { isQX: s, isLoon: o, isSurge: n, isNode: i, isJSBox: r, isScriptable: u } = ENV(); return new class { constructor(e, t) { this.name = e, this.debug = t, this.http = HTTP(), this.env = ENV(), this.node = (() => { if (i) { return { fs: require("fs") } } return null })(), this.initCache(); Promise.prototype.delay = function (e) { return this.then(function (t) { return ((e, t) => new Promise(function (s) { setTimeout(s.bind(null, t), e) }))(e, t) }) } } initCache() { if (s && (this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}")), (o || n) && (this.cache = JSON.parse($persistentStore.read(this.name) || "{}")), i) { let e = "root.json"; this.node.fs.existsSync(e) || this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.root = {}, e = `${this.name}.json`, this.node.fs.existsSync(e) ? this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)) : (this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.cache = {}) } } persistCache() { const e = JSON.stringify(this.cache, null, 2); s && $prefs.setValueForKey(e, this.name), (o || n) && $persistentStore.write(e, this.name), i && (this.node.fs.writeFileSync(`${this.name}.json`, e, { flag: "w" }, e => console.log(e)), this.node.fs.writeFileSync("root.json", JSON.stringify(this.root, null, 2), { flag: "w" }, e => console.log(e))) } write(e, t) { if (this.log(`SET ${t}`), -1 !== t.indexOf("#")) { if (t = t.substr(1), n || o) return $persistentStore.write(e, t); if (s) return $prefs.setValueForKey(e, t); i && (this.root[t] = e) } else this.cache[t] = e; this.persistCache() } read(e) { return this.log(`READ ${e}`), -1 === e.indexOf("#") ? this.cache[e] : (e = e.substr(1), n || o ? $persistentStore.read(e) : s ? $prefs.valueForKey(e) : i ? this.root[e] : void 0) } delete(e) { if (this.log(`DELETE ${e}`), -1 !== e.indexOf("#")) { if (e = e.substr(1), n || o) return $persistentStore.write(null, e); if (s) return $prefs.removeValueForKey(e); i && delete this.root[e] } else delete this.cache[e]; this.persistCache() } notify(e, t = "", a = "", h = {}) { const d = h["open-url"], l = h["media-url"]; if (s && $notify(e, t, a, h), n && $notification.post(e, t, a + `${l ? "\n多媒体:" + l : ""}`, { url: d }), o) { let s = {}; d && (s.openUrl = d), l && (s.mediaUrl = l), "{}" === JSON.stringify(s) ? $notification.post(e, t, a) : $notification.post(e, t, a, s) } if (i || u) { const s = a + (d ? `\n点击跳转: ${d}` : "") + (l ? `\n多媒体: ${l}` : ""); if (r) { require("push").schedule({ title: e, body: (t ? t + "\n" : "") + s }) } else console.log(`${e}\n${t}\n${s}\n\n`) } } log(e) { this.debug && console.log(`[${this.name}] LOG: ${this.stringify(e)}`) } info(e) { console.log(`[${this.name}] INFO: ${this.stringify(e)}`) } error(e) { console.log(`[${this.name}] ERROR: ${this.stringify(e)}`) } wait(e) { return new Promise(t => setTimeout(t, e)) } done(e = {}) { s || o || n ? $done(e) : i && !r && "undefined" != typeof $context && ($context.headers = e.headers, $context.statusCode = e.statusCode, $context.body = e.body) } stringify(e) { if ("string" == typeof e || e instanceof String) return e; try { return JSON.stringify(e, null, 2) } catch (e) { return "[object Object]" } } }(e, t) }
